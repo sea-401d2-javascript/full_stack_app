47948fa2a8f857ea477197ea85740f9a619e7dec
Sawako Ishida <sawako.yoriko@gmail.com>
2016-04-08 07:46:16 -0700

    initial commit with rest-api routes

---
+++ full-stack-sawako/models/whatever_model.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const WhateverSchema = new mongoose.Schema({
+  
+});
---
+++ full-stack-sawako/routes/route3.js
@@ -0,0 +1,6 @@
+// 'use strict';
+// var express = require('express');
+// var whateverRouter = express.Router();
+// // var bodyParser = require('body-parser');
+// // var Gem = require('./models/gem_model');
+// var Continent = require('./models/continent_model');
