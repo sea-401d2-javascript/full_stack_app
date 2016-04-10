8db2a100ac9b542099fc4acbd6b347c36b9e310d
nienowt <elstad.nienow@gmail.com>
2016-04-10 16:06:48 -0700

    cancel edit works

--- nienowt/controllers/ghost-controller.js
+++ nienowt/controllers/ghost-controller.js
@@ -6,11 +6,13 @@ angular.module('app',[])
     const mainRoute = 'http://localhost:3000/api/ghosts';
     this.angTest = 'TOO GOOD ???';
     this.ghosts = ['ghost'];
+
     this.confirmChange = function(ghost, buttonName, curGhost){
       if (!$scope.editConfirmation) return $scope.editConfirmation = true;
       if(buttonName === 'delete') return this.removeGhost(ghost);
       if(buttonName === 'edit') return this.editGhost(ghost, curGhost);
     }
+
     this.getGhosts = function() {
       $http.get(mainRoute)
       .then((results) => {
@@ -21,14 +23,16 @@ angular.module('app',[])
         if (err) console.log(err);
       })
     }
+
     this.createGhost = function(ghost) {
       $http.post('http://localhost:3000/pub/new-ghost', ghost)
       .then((res) => {
         this.ghosts.push(ghost)
-        $scope.newGhost = {}
+        $scope.newGhost = {};
         $scope.newForm.$setPristine();
       })
     }
+
     this.editGhost = function(changedGhost, ghost){
       $http.put(mainRoute + '/' + ghost._id, changedGhost)
       .then((res) => {
@@ -36,10 +40,17 @@ angular.module('app',[])
         this.ghosts.push(changedGhost);
       })
     }
+
     this.removeGhost = function(ghost){
       $http.delete(mainRoute + '/' + ghost._id)
       .then((res) => {
         this.ghosts = this.ghosts.filter((g) => g._id != ghost._id);
       })
     }
+
+    // this.reset = function(changedGhost){
+    //   console.log('button being clicked');
+    //   $scope.changedGhost = {};
+    //   $scope.editShow = false;
+    // }
   }])
