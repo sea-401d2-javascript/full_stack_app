72e8303033e6a2a759b18d62982fdf7e868f86e7
Sawako Ishida <sawako.yoriko@gmail.com>
2016-04-11 11:30:43 -0700

    updated delete feature delete is using an existing array data to display on the browser instead of useing data that's being sent directly from the server.

--- full-stack-sawako/cli-backend/build/bundle.js
+++ full-stack-sawako/cli-backend/build/bundle.js
@@ -56,6 +56,7 @@
 	  var self = this;
 	  this.continentsList = [];
 	  this.continents = [];
+	  this.allContinents = [];
 	  this.newConts = {
 	    country: '',
 	    region: '',
@@ -65,17 +66,26 @@
 	  this.id = ''
 	  this.editing = false;
 	  this.buttonShow = false;
+	  this.deleting = false;
 	  this.fetchedData = [];
 	  this.cancelEdits = function(){
 	    this.getCont = this.fetchedData;
 	    console.log('back to original : ' + angular.toJson(this.fetchedData));
 	  }
 
+	  this.removeContFromArr = function(){
+	      this.allContinents = this.allContinents.filter((data)=>{
+	        return data._id !== this.id;
+	      });
+	  };
+
 	  this.getContinents = function(){
 	    $http.get(mainRoute)
 	    .then((result)=>{
 	      console.log('Here is result ' + result);
 	      this.continentsList = result.data;
+	      this.allContinents = angular.copy(result.data);
+	      console.log('Lets use them later : ' + this.allContinents);
 	    }, function(err){
 	      console.log(err);
 	    })
@@ -113,6 +123,10 @@
 	  }
 
 	  this.deleteContinentById = function(){
+	    this.deleting = true;
+	    this.removeContFromArr();
+	    console.log('Filtered array? : ' + angular.toJson(this.allContinents));
+	    console.log(this.deleting);
 	    $http.delete(mainRoute + '/'+ this.id, this.getCont)
 	    .then((result)=>{
 	      this.getCont = this.data;
@@ -133,6 +147,12 @@
 	  this.getGem = {}
 	  this.id = ''
 	  this.editing = false;
+	  this.buttonShow = false;
+	  this.fetchedData = [];
+	  this.cancelEdits = function(){
+	    this.getGem = this.fetchedData;
+	    console.log('back to original : ' + angular.toJson(this.fetchedData));
+	  }
 
 	  this.getGems = function(){
 	    $http.get(mainRoute)
@@ -144,6 +164,16 @@
 	    });
 	  }
 
+	  this.getGemById = function(){
+	      $http.get(mainRoute + '/' + this.id)
+	      .then((result)=>{
+	        this.getGem = result.data;
+	        this.fetchedData = angular.copy(result.data);
+	      }, function(err){
+	        console.log('Here is err : ' + err);
+	      })
+	  }
+
 	  this.createGems = function(){
 	    $http.post(mainRoute, this.newGem)
 	    .then((result)=>{
@@ -151,6 +181,26 @@
 	      console.log('Here is new Gem! : ' + this.gems);
 	    }, function(err){
 	      console.log('Err : ' + err);
+	    })
+	  }
+
+	  this.editGem = function(){
+	    $http.put(mainRoute + '/' + this.id, this.getGem)
+	    .then((result)=>{
+	      this.getGem = this.data;
+	      this.status = 'Successfully updated : ' + angular.toJson(this.getGem);
+	    }, function(err){
+	      console.log('Here is err : ' + err);
+	    })
+	  }
+
+	  this.deleteGemById = function(){
+	    $http.delete(mainRoute + '/' + this.id, this.getGem)
+	    .then((result)=>{
+	      this.getGem = result.data;
+	      this.status = 'Successfully deleted : ' + angular.toJson(this.getGem);
+	    }, function(err){
+	      console.log('Here is err : ' + err);
 	    })
 	  }
 	}])
