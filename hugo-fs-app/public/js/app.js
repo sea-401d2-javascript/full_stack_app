723f6acf7fa3ddd62094f5088fca2a1a8356ce8e
Hugo A Guerra <guerrahugoa@gmail.com>
2016-04-10 21:27:55 -0700

    added button styling, added list layout, added form styling, added header element and styling, brought in new fonts, added drop shadows, random color changes

--- hugo-fs-app/public/js/app.js
+++ hugo-fs-app/public/js/app.js
@@ -1,6 +1,8 @@
 require(__dirname + '/../../bower_components/angular/angular.js');
+require(__dirname + '/../css/normalize.css');
 require(__dirname + '/../css/main.css');
-//needs put routes!!!!
+require(__dirname + '/../css/animate.css');
+
 const app = angular.module('CustomerApp', []);
 app.controller('CustomerController', ['$http', function($http) {
   const customersRoute = 'http://localhost:3000/customers';
