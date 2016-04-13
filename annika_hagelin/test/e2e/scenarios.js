53f2fc5ff12632ce00a0b3ca4b938b135053f1e8
ankihg <hagelin.annika@gmail.com>
2016-04-13 10:38:33 -0700

    species repeat testing

--- annika_hagelin/test/e2e/scenarios.js
+++ annika_hagelin/test/e2e/scenarios.js
@@ -1,12 +1,26 @@
-describe('trees and species testing', function() {
-  var resource = element(by.binding('speciessCtrl.resource'));
+describe('end to end testing', function() {
 
-  beforeEach(function() {
-    browser.get('http://localhost:8080');
-  });
+  describe('speciess testing', function() {
+    var resource = element(by.binding('speciessCtrl.resource'));
+    var speciessRepeater = element.all(by.repeater('species in speciessCtrl.speciess')); //by.repeater('species in speciessCtrl.speciess');
 
-  it('have correct resource', function() {
-    expect(resource.getText()).toEqual('speciess'.toUpperCase());
+    beforeEach(function() {
+      browser.get('http://localhost:8080');
+    });
+
+    it('have correct resource', function() {
+      expect(resource.getText()).toEqual('speciess'.toUpperCase());
+    });
+
+    it('species have column genus', function() {
+      var species0 = element.all(by.repeater('species in speciessCtrl.speciess').column('species.genus')).getText();
+      species0.then(function(genera) {
+        genera.forEach(function(genus) {
+          expect(genus).toBeDefined();
+        });
+      });
+    });
+
   });
 
 });
