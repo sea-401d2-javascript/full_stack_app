aca578e55265c7f03dfb405302033d3c8584c5ed
samanthaprince928@gmail.com <samanthaprince928@gmail.com>
2016-04-14 12:25:21 -0700

    add unit tests for recipes:

--- samantha/test/unit/client-spec.js
+++ samantha/test/unit/client-spec.js
@@ -70,3 +70,72 @@ describe('it should test something', () => {
     });
   });
 });
+
+describe('it should test something', () => {
+  var RecipeController;
+  it('should have a test', () => {
+    expect(false).toBe(false);
+  });
+
+  beforeEach(angular.mock.module('ChefApp'));
+  beforeEach(angular.mock.inject(function($controller) {
+    RecipeController = $controller('RecipeController');
+  }));
+
+  it('should construct a controller', ()=> {
+    expect(typeof RecipeController).toBe('object');
+    expect(typeof RecipeController.createRecipe).toBe('function');
+  });
+  describe('REST tests', ()=> {
+    var $httpBackend;
+
+    beforeEach(angular.mock.inject(function(_$httpBackend_){
+      $httpBackend = _$httpBackend_;
+    }));
+
+    afterEach(()=> {
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('should get all recipes', ()=> {
+      $httpBackend.expectGET('http://localhost:3000/recipes')
+        .respond(200, {data: [{name: 'test recipe'}]});
+      RecipeController.getRecipes();
+      $httpBackend.flush();
+      expect(RecipeController.recipes.length).toBe(1);
+      expect(RecipeController.recipes[0].name).toBe('test recipe');
+    });
+
+    it('should create a new recipe', ()=> {
+      $httpBackend.expectPOST('http://localhost:3000/recipes', {name: 'test recipe name'})
+        .respond(200, {name: 'test recipe name', cookTime: 30, _id:'uniqueid'});
+      RecipeController.createRecipe({name: 'test recipe name'});
+      $httpBackend.flush();
+      expect(RecipeController.recipes.length).toBeGreaterThan(0);
+      expect(RecipeController.recipes[1].name).toBe('test recipe name');
+      // expect(ChefController.newChef).toBe('object');
+    });
+
+    it('should update a recipe', ()=> {
+      $httpBackend.expectPUT('http://localhost:3000/recipes/2')
+        .respond(200, {data: [{name: 'test recipe', cookTime: 30, _id: 2}]});
+      RecipeController.recipes.push({name: 'test chef', cookTime: 30, _id: 2});
+      RecipeController.updateRecipe({name: 'updated recipe', cookTime: 45, _id: 2});
+      $httpBackend.flush();
+      expect(RecipeController.recipes.length).toBe(2);
+      expect(RecipeController.recipes.every((r) => r._id = 2)).toBe(true);
+      // expect(ChefController.chefs[1].name).toBe('updated chef');
+    });
+
+    it('should delete a recipe', ()=> {
+      $httpBackend.expectDELETE('http://localhost:3000/recipes/3')
+        .respond(200);
+      RecipeController.recipes.push({name: 'test recipe', _id: 3});
+      RecipeController.removeRecipe({name: 'test recipe', _id:3});
+      $httpBackend.flush();
+      expect(RecipeController.recipes.length).toBe(1);
+      expect(RecipeController.recipes.every((r)=> r._id != 3)).toBe(true);
+    });
+  });
+});
