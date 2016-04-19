aa10a9ea57bb09374a5cdf03f14285f06971f3fc
Lex Myers <lex.myers@gmail.com>
2016-04-18 20:57:15 -0700

    latest changes

--- lexm/test/e2e/movie_e2e.js
+++ lexm/test/e2e/movie_e2e.js
@@ -1,7 +1,11 @@
 describe('movie section', function() {
-  var newMovie = element(by.model('newMovie.name'));
+  var newMovieName = element(by.model('newMovie.name'));
   var newMovieReleaseDate = element(by.model('newMovie.release_date'));
   var newMovieButton = element(by.buttonText('New Movie'));
+  var deleteButton = element(by.buttonText('Delete'));
+  var editButton = element(by.buttonText('Edit'));
+  var editMovieName = element(by.model('movEdit.name'));
+  var editMovieReleaseDate = element(by.model('movEdit.release_date'));
 
   beforeEach(function() {
     browser.get('http://127.0.0.1:8080');
@@ -12,6 +16,22 @@ describe('movie section', function() {
   });
 
   it('should start out blank', function() {
-    expect(newMovie.getAttribute('value')).toEqual('');
+    expect(newMovieName.getAttribute('value')).toEqual('');
+  });
+
+  it('should be able to add a movie', function() {
+    newMovieName.clear();
+    newMovieName.sendKeys('The Godfather');
+    newMovieReleaseDate.sendKeys('12201974');
+    newMovieButton.click();
+
+    expect(newMovieName.getAttribute('value')).toEqual('The Godfather');
+    expect(newMovieReleaseDate.getAttribute('value')).toEqual('1974-12-20');
+  });
+
+  it('should be able to bring up the edit window', function() {
+    editButton.click();
+    expect(editMovieName.getAttribute('value')).toEqual('');
+    expect(editMovieReleaseDate.getAttribute('value')).toEqual('');
   });
 });
