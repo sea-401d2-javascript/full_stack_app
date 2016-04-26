fa4c98f961f64613019dcd2826d4f3e036e59145
Hugo A Guerra <guerrahugoa@gmail.com>
2016-04-26 11:10:13 -0700

    fully initialized service

--- hugo-fs-app/public/js/app.js
+++ hugo-fs-app/public/js/app.js
@@ -8,6 +8,7 @@ require(__dirname + '/customers/customer-module.js');
 require(__dirname + '/customers/customers.js');
 require(__dirname + '/products/products-module.js');
 require(__dirname + '/products/products.js');
+require(__dirname + '/services/http-service.js');
 
 (function() {
   angular.module('app', [
