74a4f51f5cce0e72ec5529a72b1aa741aa496809
Mikleane Chouinard <mikleane@msn.com>
2016-04-27 09:05:22 -0700

    stylesheet adjustments

--- mikleane/js/index.js
+++ mikleane/js/index.js
@@ -1,7 +1,7 @@
 (function(){
 
   let angular = require('angular');
-  require(__dirname + '/../build/beaut.css')
+  
   require(__dirname + '/beers/beers_module.js')
   require(__dirname + '/beers/beers.js')
   require(__dirname + '/users/users_module.js')
