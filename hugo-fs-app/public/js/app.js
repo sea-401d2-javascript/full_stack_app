6f8aa73c84c4e825e200cd7fc69d875264e95a06
Hugo A Guerra <guerrahugoa@gmail.com>
2016-04-27 12:00:05 -0700

    added route functionality, fully working

--- hugo-fs-app/public/js/app.js
+++ hugo-fs-app/public/js/app.js
@@ -1,6 +1,8 @@
 'use strict';
 
 require(__dirname + '/../../bower_components/angular/angular.js');
+// require(__dirname + '/../../bower_components/angular/angular-route.js');
+require('angular-route');
 require(__dirname + '/../css/normalize.css');
 require(__dirname + '/../css/main.css');
 require(__dirname + '/../css/animate.css');
@@ -10,9 +12,20 @@ require(__dirname + '/products/products-module.js');
 require(__dirname + '/products/products.js');
 require(__dirname + '/services/http-service.js');
 
+
 (function() {
   angular.module('app', [
     'customers',
-    'products'
-  ]);
+    'products',
+    'ngRoute'
+  ])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/customers', {
+      templateUrl: './templates/customer-template.html'
+    }).when('/products', {
+      templateUrl: './templates/product-template.html'
+    }).when('/', {
+      templateUrl: './templates/customer-template.html'
+    })
+  }]);
 })();
